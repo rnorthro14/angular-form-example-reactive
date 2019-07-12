359e8deab8c69f0705cff093dd04eeeebc4b552e
administrator <administrator@administrators-MacBook-Pro.local>
2019-07-12 09:51:35 -0600

    initial commit with basic angular 7 reactive form features

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customers/customer.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
